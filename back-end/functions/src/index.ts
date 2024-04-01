c9843bbb5e3a0b25e6191bc41bd77d9bf4d8bba3
Jonathan Hop <yithril@gmail.com>
2024-04-01 11:29:30 -0400

    build changes

--- back-end/functions/src/index.ts
+++ back-end/functions/src/index.ts
@@ -1,19 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = express();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+app.get('/api/hello', (req, res) => {
+  res.send('Hello from Firebase!');
+});
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Correct export syntax for TypeScript
+export const api = functions.https.onRequest(app);
